b5b820f65e648c8ea36518963445b0e25e94fe22
Kiran Anand <liteninkiran@hotmail.com>
2022-11-08 22:08:04 +0000

    Display bookings

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
     selector: 'app-rooms',
@@ -9,7 +10,41 @@ export class RoomsComponent implements OnInit {
 
     public hotelName = 'Hilton Hotel';
     public numberOfRooms = 10;
-    public hideRooms = false;
+    public hideRooms = true;
+    public rooms: Room = {
+        availableRooms: 10,
+        bookedRooms: 5,
+        totalRooms: 20,
+    };
+    public roomList: RoomList[] = [
+        {
+            roomNumber: 1,
+            roomType: 'Deluxe Room',
+            amenities: 'Air Conditioning, Wi-Fi, TV, Bathroom, Kitchen',
+            price: 500,
+            photos: 'https://unsplash.com/photos/oxeCZrodz78',
+            checkinTime: new Date('11-NOV-2021'),
+            checkoutTime: new Date('12-NOV-2021'),
+        },
+        {
+            roomNumber: 2,
+            roomType: 'Standard Room',
+            amenities: 'Air Conditioning, Wi-Fi, TV, Bathroom, Kitchen',
+            price: 200,
+            photos: 'https://unsplash.com/photos/PJNO2sLlbB8',
+            checkinTime: new Date('11-NOV-2021'),
+            checkoutTime: new Date('12-NOV-2021'),
+        },
+        {
+            roomNumber: 3,
+            roomType: 'Double Room',
+            amenities: 'Air Conditioning, Wi-Fi, TV, Bathroom, Kitchen',
+            price: 300,
+            photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
+            checkinTime: new Date('11-NOV-2021'),
+            checkoutTime: new Date('12-NOV-2021'),
+        },
+    ];
 
     constructor() { }
 
